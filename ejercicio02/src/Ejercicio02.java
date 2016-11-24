4eeac8d99fb47dd4a16bc17337985eec03f958b6
Patricio José <soriwert@gmail.com>
2016-11-24 19:27:12 +0100

    24/11/2016

--- ejercicio02/src/Ejercicio02.java
+++ ejercicio02/src/Ejercicio02.java
@@ -4,28 +4,39 @@ public class Ejercicio02{
 	public static void main (String[] args){
 		Scanner teclado = new Scanner (System.in);
 		String frase;
-		minuscula=0;
-		mayuscula=0;
-		int i=0;
+		int i, mayus=0, minus=0;
+		char letra;
+		frase.
 
-		System.out.println("Escriba una frase");
+		System.out.print("Introduce una frase: ");
 		frase=teclado.nextLine();
 
-		while (frase.length()>=i){
-			System.out.println(frase.charAt(i));
-			if (){
-				minuscula++;
+		while (frase.length() > 80){
+			System.out.println("Has introducido mas de 80 caracteres. Introduce una frase: ");
+			frase=teclado.nextLine();
+		}
+
+		for (i=0; i<frase.length(); i++){
+			letra=frase.charAt(i);
+			//System.out.println(letra);
+			if (letra>='a'&& letra <='z'){
+				minus++;
 			}
-			else if (){
-				mayuscula++;
+			else if (letra >='A'&&letra<='Z'){
+					mayus++;
+				
 			}
 		}
+		System.out.println("mayus = "+mayus);
+		System.out.println("minus = "+minus);
+
 
 
 	}
-
-
 }
+
+
+
 
 
 
