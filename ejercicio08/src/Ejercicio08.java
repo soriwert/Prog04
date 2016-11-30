6d29e70a97daf40e22fddfc1d28cc1476bac424f
Soriano <Soriano@DESKTOP-7QTFTDI>
2016-12-01 00:56:57 +0100

    casa

--- ejercicio08/src/Ejercicio08.java
+++ ejercicio08/src/Ejercicio08.java
@@ -36,77 +36,78 @@ public class Ejercicio08 {
 			supera=0;
 			if (((int)frase.charAt(i)+n)>=(int)'A'&&((int)frase.charAt(i)+n)<=(int)'Z'){
 				fraseCode+=(int)frase.charAt(i)+n;
-				System.out.print("\n1 Muestra "+(char)(fraseCode)+" porfa");
-				
+				System.out.print((char)(fraseCode));
+			
 			}
 			
 			//codificado fuera del margen por debajo
-			
-			else if ((int)frase.charAt(i)>=49&&(int)frase.charAt(i)<65){
-				supera=(int)frase.charAt(i)-49+calculo;
-				System.out.print("2 Muestra "+(char) supera+" porfa");
-			}
-			
-			else if ((int)frase.charAt(i)>=33){
-				supera=49-(int)frase.charAt(i)+calculo;
-				System.out.print("3 Muestra "+(char) supera+" porfa");
-				
-			}
 			else if ((int)frase.charAt(i)==32){
 				supera=33-(int)frase.charAt(i)+calculo;
-				System.out.print("4 Muestra "+(char) supera+" porfa");
+				System.out.print((char)supera);
+			
+			}
+			else if ((int)frase.charAt(i)<49){
+				supera=49-(int)frase.charAt(i)+calculo;
+				System.out.print((char) supera);
+				
+			}
+						
+			else if ((int)frase.charAt(i)>=49&&(int)frase.charAt(i)<65){
+				supera=(int)frase.charAt(i)-49+calculo;
+				System.out.print((char) supera);
+				
 			}
 			
 			//codificado fuera del margen por arriba			
 			else if ((int)frase.charAt(i)<=106){
 				supera=(int)frase.charAt(i)-90+calculo;
-				System.out.print("5 Muestra "+(char) supera+" porfa");
+				System.out.print((char) supera);
 				
 			}
 						
 			else if ((int)frase.charAt(i)<=122){
 				supera=(int)frase.charAt(i)-106+calculo;
-				System.out.print("6 Muestra "+(char) supera+" porfa");
+				System.out.print((char) supera);
 				
 			}
 			else if ((int)frase.charAt(i)<=138){
 				supera=(int)frase.charAt(i)-122+calculo;
-				System.out.print("7 Muestra "+(char) supera+" porfa");
+				System.out.print((char) supera);
 				
 			}
 			else if ((int)frase.charAt(i)<=154){
 				supera=(int)frase.charAt(i)-138+calculo;
-				System.out.print("8 Muestra "+(char) supera+" porfa");
+				System.out.print((char) supera);
 				
 			}
 			else if ((int)frase.charAt(i)<=170){
 				supera=(int)frase.charAt(i)-154+calculo;
-				System.out.print("9 Muestra "+(char) supera+" porfa");
+				System.out.print((char) supera);
 				
 			}
 			else if ((int)frase.charAt(i)<=186){
 				supera=(int)frase.charAt(i)-170+calculo;
-				System.out.print("10 Muestra "+(char) supera+" porfa");
+				System.out.print((char) supera);
 				
 			}
 			else if ((int)frase.charAt(i)<=202){
 				supera=(int)frase.charAt(i)-186+calculo;
-				System.out.print("11 Muestra "+(char) supera+" porfa");
+				System.out.print((char) supera);
 				
 			}
 			else if ((int)frase.charAt(i)<=218){
 				supera=(int)frase.charAt(i)-202+calculo;
-				System.out.print("12 Muestra "+(char) supera+" porfa");
+				System.out.print((char) supera);
 				
 			}
 			else if ((int)frase.charAt(i)<=234){
 				supera=(int)frase.charAt(i)-218+calculo;
-				System.out.print("13 Muestra "+(char) supera+" porfa");
+				System.out.print((char) supera);
 				
 			}
 			else if ((int)frase.charAt(i)<=250){
 				supera=(int)frase.charAt(i)-234+calculo;
-				System.out.print("14 Muestra "+(char) supera+" porfa");
+				System.out.print((char) supera);
 				
 			}
 				else
